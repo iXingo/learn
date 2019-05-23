e16d080e7be020666870a171314018cc946338f5
Shawn Wang <shawang@nvidia.com>
2019-05-23 19:41:35 +0800

    add local date

---
+++ src/main/java/com/xindog/datatime/DataTimeExample.java
@@ -0,0 +1,24 @@
+package com.xindog.datatime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * Created by Xindog.com(TM).
+ * Author:  Shawn.Wang / i.am@shawn.wang
+ * Date:    5/23/19
+ * Time:    10:43 AM
+ * Project: learn
+ */
+public class DataTimeExample {
+
+    public static void main(String[] args) {
+        LocalDateTime nowTime = LocalDateTime.of(LocalDate.of(2019,05,22 ), LocalTime.of(0,06,41));
+
+        LocalDateTime nextTime = nowTime.minusWeeks(2);
+
+        System.out.print("\n"+nextTime+"Z ");
+        System.out.println(nowTime+"Z");
+    }
+}
