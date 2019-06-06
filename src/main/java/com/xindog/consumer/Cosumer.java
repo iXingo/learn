2d9150443a306535df544998ad128ec40738ce35
Shawn Wang <ixingo@126.com>
2019-06-06 23:58:00 +0800

    update test

---
+++ src/main/java/com/xindog/consumer/Cosumer.java
@@ -0,0 +1,16 @@
+package com.xindog.consumer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Cosumer {
+
+
+
+    public static void main(String[] args){
+        new Thread(()->{
+            while(true){
+            }
+        }).start();
+    }
+}
