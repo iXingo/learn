ebae8637847092841ee766b17a8b2b8150b40a1a
Shawn Wang <ixingo@126.com>
2019-07-08 22:27:36 +0800

    update Netty server

---
+++ src/main/java/com/xindog/bilibili/MyServerHandler.java
@@ -0,0 +1,27 @@
+package com.xindog.bilibili;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + "," + msg);
+        ctx.channel().writeAndFlush("from server: "+ UUID.randomUUID());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush("Hello World@");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
