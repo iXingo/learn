f404d49e72738f21638fe11a60388267a8ea610c
Shawn Wang <ixingo@126.com>
2019-07-09 23:24:40 +0800

    update websocket server

--- src/main/java/com/xindog/bilibili/scoket/WebSocketServerhandler.java
+++ src/main/java/com/xindog/bilibili/scoket/WebSocketServerhandler.java
@@ -1,14 +1,46 @@
 package com.xindog.bilibili.scoket;
 
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.HttpObject;
-import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import org.joda.time.LocalDateTime;
 
-public class WebSocketServerhandler extends SimpleChannelInboundHandler<HttpObject> {
+public class WebSocketServerhandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("收到消息" + msg.text());
 
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间" + LocalDateTime.now()));
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Handler Added" + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Handler Removed" + ctx.channel().id().asLongText());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("链接开启" + ctx.channel().id().asLongText());
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("链接开启" + LocalDateTime.now()));
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("链接关闭" + ctx.channel().id().asLongText());
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("链接关闭" + LocalDateTime.now()));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("异常发生");
+        cause.printStackTrace();
+        ctx.channel().close();
     }
 }
--- src/main/java/com/xindog/stream/RunStream.java
+++ src/main/java/com/xindog/stream/RunStream.java
@@ -22,46 +22,46 @@ import java.util.stream.Stream;
  */
 public class RunStream {
 
-	public static void main(String[] args) {
-		Random random = new Random();
-		// 随机产生数据
-		Stream<Integer> stream = Stream.generate(() -> random.nextInt())
-				// 产生500个 ( 无限流需要短路操作. )
-				.limit(500)
-				// 第1个无状态操作
-				.peek(s -> print("peek: " + s))
-				// 第2个无状态操作
-				.filter(s -> {
-					print("filter: " + s);
-					return s > 1000000;
-				})
-				// 有状态操作
-				.sorted((i1, i2) -> {
-					print("排序: " + i1 + ", " + i2);
-					return i1.compareTo(i2);
-				})
-				// 又一个无状态操作
-				.peek(s -> {
-					print("peek2: " + s);
-				}).parallel();
+    public static void main(String[] args) {
+        Random random = new Random();
+        // 随机产生数据
+        Stream<Integer> stream = Stream.generate(() -> random.nextInt())
+                // 产生500个 ( 无限流需要短路操作. )
+                .limit(500)
+                // 第1个无状态操作
+                .peek(s -> print("peek: " + s))
+                // 第2个无状态操作
+                .filter(s -> {
+                    print("filter: " + s);
+                    return s > 1000000;
+                })
+                // 有状态操作
+                .sorted((i1, i2) -> {
+                    print("排序: " + i1 + ", " + i2);
+                    return i1.compareTo(i2);
+                })
+                // 又一个无状态操作
+                .peek(s -> {
+                    print("peek2: " + s);
+                }).parallel();
 
-		// 终止操作
-		stream.count();
-	}
+        // 终止操作
+        stream.count();
+    }
 
-	/**
-	 * 打印日志并sleep 5 毫秒
-	 *
-	 * @param s
-	 */
-	public static void print(String s) {
-		// System.out.println(s);
-		// 带线程名(测试并行情况)
-		System.out.println(Thread.currentThread().getName() + " > " + s);
-		try {
-			TimeUnit.MILLISECONDS.sleep(5);
-		} catch (InterruptedException e) {
-		}
-	}
+    /**
+     * 打印日志并sleep 5 毫秒
+     *
+     * @param s
+     */
+    public static void print(String s) {
+        // System.out.println(s);
+        // 带线程名(测试并行情况)
+        System.out.println(Thread.currentThread().getName() + " > " + s);
+        try {
+            TimeUnit.MILLISECONDS.sleep(5);
+        } catch (InterruptedException e) {
+        }
+    }
 
 }
