29068142672750e2c5956471c861d76c9584d6ce
Shawn Wang <shawang@nvidia.com>
2019-07-22 17:13:47 +0800

    Data related

--- src/main/java/com/xindog/jdk8/joda/JodaTest1.java
+++ src/main/java/com/xindog/jdk8/joda/JodaTest1.java
@@ -1,17 +1,17 @@
 package com.xindog.jdk8.joda;
 
 
-import org.joda.primitives.list.IntList;
-import org.joda.primitives.list.impl.ArrayIntList;
+//import org.joda.primitives.list.IntList;
+//import org.joda.primitives.list.impl.ArrayIntList;
 
 public class JodaTest1 {
 
-    public static void main(String[] args) {
-        IntList intList = new ArrayIntList();
-
-        intList.add(1);
-        intList.add(2);
-
-        intList.forEach(System.out::println);
-    }
+//    public static void main(String[] args) {
+//        IntList intList = new ArrayIntList();
+//
+//        intList.add(1);
+//        intList.add(2);
+//
+//        intList.forEach(System.out::println);
+//    }
 }
