6f0811bbe48fe35279b951563a94a0cfc3f45cf2
Shawn Wang <ixingo@126.com>
2020-12-20 23:08:15 +0800

    add vertx in action

---
+++ src/main/java/com/xindog/vertx/chapter1/snippets/SynchronousEcho.java
@@ -0,0 +1,41 @@
+package com.xindog.vertx.chapter1.snippets;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@Slf4j
+public class SynchronousEcho {
+
+  public static void main(String[] args) throws Throwable {
+    ServerSocket server = new ServerSocket();
+    server.bind(new InetSocketAddress(3000));
+    while (true) {   // <1>
+      Socket socket = server.accept();
+      new Thread(clientHandler(socket)).start();
+    }
+  }
+
+  private static Runnable clientHandler(Socket socket) {
+    return () -> {
+      try (
+        BufferedReader reader = new BufferedReader(
+          new InputStreamReader(socket.getInputStream()));
+        PrintWriter writer = new PrintWriter(
+          new OutputStreamWriter(socket.getOutputStream()))) {
+        String line = "";
+        while (!"/quit".equals(line)) {
+          line = reader.readLine();      // <2>
+          log.info("~ " + line);
+          writer.write(line + "\n");  // <3>
+          writer.flush();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    };
+  }
+}
