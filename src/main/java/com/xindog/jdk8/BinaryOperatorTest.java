f8639fa182b646307a16865219c06e8afd77442d
Shawn Wang <ixingo@126.com>
2021-01-09 23:50:29 +0800

    use log rather than sout

--- src/main/java/com/xindog/jdk8/BinaryOperatorTest.java
+++ src/main/java/com/xindog/jdk8/BinaryOperatorTest.java
@@ -1,21 +1,25 @@
 package com.xindog.jdk8;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Comparator;
 import java.util.function.BinaryOperator;
 
+
+@Slf4j
 public class BinaryOperatorTest {
 
     public static void main(String[] args) {
         BinaryOperatorTest binaryOperatorTest = new BinaryOperatorTest();
 
-        System.out.println(binaryOperatorTest.compute(1, 2, Integer::sum));
-        System.out.println(binaryOperatorTest.compute(1, 2, (a, b) -> a - b));
+        log.info("binaryOperator Test Result => {}", binaryOperatorTest.compute(1, 2, Integer::sum));
+        log.info("binaryOperator Test Result => {}", binaryOperatorTest.compute(1, 2, (a, b) -> a - b));
 
-        System.out.println("----------");
+        log.info("----------");
 
-        System.out.println(binaryOperatorTest.getShort("hello123", "world", Comparator.comparingInt(String::length)));
-        System.out.println(binaryOperatorTest.getShort("hello123", "world", Comparator.comparingInt(a -> a.charAt(0))));
+        log.info("binaryOperator Test Result => {}", binaryOperatorTest.getShort("hello123", "world", Comparator.comparingInt(String::length)));
+        log.info("binaryOperator Test Result => {}", binaryOperatorTest.getShort("hello123", "world", Comparator.comparingInt(a -> a.charAt(0))));
 
     }
 
