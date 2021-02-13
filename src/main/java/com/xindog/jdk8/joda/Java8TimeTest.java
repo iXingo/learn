5bffb05c4e92f1970612f3c3f61ea0ace203c874
Shawn Wang <ixingo@126.com>
2021-02-13 23:57:57 +0800

    use log rather than system.out.println

--- src/main/java/com/xindog/jdk8/joda/Java8TimeTest.java
+++ src/main/java/com/xindog/jdk8/joda/Java8TimeTest.java
@@ -1,72 +1,75 @@
 package com.xindog.jdk8.joda;
 
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Set;
 import java.util.TreeSet;
 
+@Slf4j
 public class Java8TimeTest {
 
     public static void main(String[] args) {
 
         Date date = new Date(System.currentTimeMillis());
         LocalDate localDate = LocalDate.now();
-        System.out.println(localDate);
+        log.info(String.valueOf(localDate));
 
-        System.out.println(localDate.getYear() + ", " + localDate.getMonthValue() + ", " + localDate.getDayOfMonth());
+        log.info(localDate.getYear() + ", " + localDate.getMonthValue() + ", " + localDate.getDayOfMonth());
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalDate localDate2 = LocalDate.of(2017, 3, 3);
-        System.out.println(localDate2);
+        log.info(String.valueOf(localDate2));
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalDate localDate3 = LocalDate.of(2010, 3, 25);
         MonthDay monthDay = MonthDay.of(localDate3.getMonth(), localDate3.getDayOfMonth());
         MonthDay monthDay2 = MonthDay.from(LocalDate.of(2011, 3, 26));
 
         if (monthDay.equals(monthDay2)) {
-            System.out.println("equals");
+            log.info("equals");
         } else {
-            System.out.println("not equals");
+            log.info("not equals");
         }
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalTime time = LocalTime.now();
-        System.out.println(time);
+        log.info(String.valueOf(time));
 
         LocalTime time2 = time.plusHours(3).plusMinutes(20);
-        System.out.println(time2);
+        log.info(String.valueOf(time2));
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalDate localDate1 = localDate.plus(2, ChronoUnit.WEEKS);
-        System.out.println(localDate1);
+        log.info(String.valueOf(localDate1));
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalDate localDate4 = localDate.minus(2, ChronoUnit.MONTHS);
-        System.out.println(localDate4);
+        log.info(String.valueOf(localDate4));
 
-        System.out.println("-------");
+        log.info("-------");
 
         Clock clock = Clock.systemDefaultZone();
-        System.out.println(clock.millis());
+        log.info(String.valueOf(clock.millis()));
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalDate localDate5 = LocalDate.now();
         LocalDate localDate6 = LocalDate.of(2017, 3, 18);
 
-        System.out.println(localDate5.isAfter(localDate6));
-        System.out.println(localDate5.isBefore(localDate6));
-        System.out.println(localDate5.equals(localDate6));
+        log.info(String.valueOf(localDate5.isAfter(localDate6)));
+        log.info(String.valueOf(localDate5.isBefore(localDate6)));
+        log.info(String.valueOf(localDate5.equals(localDate6)));
 
-        System.out.println("-------");
+        log.info("-------");
 
         Set<String> set = ZoneId.getAvailableZoneIds();
         Set<String> treeSet = new TreeSet<String>() {
@@ -76,52 +79,52 @@ public class Java8TimeTest {
         };
         treeSet.stream().forEach(System.out::println);
 
-        System.out.println("-------");
+        log.info("-------");
 
         ZoneId zoneId = ZoneId.of("Asia/Shanghai");
         LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(localDateTime);
+        log.info(String.valueOf(localDateTime));
 
         ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, zoneId);
-        System.out.println(zonedDateTime);
+        log.info(String.valueOf(zonedDateTime));
 
-        System.out.println("-------");
+        log.info("-------");
 
         YearMonth yearMonth = YearMonth.now();
-        System.out.println(yearMonth);
-        System.out.println(yearMonth.lengthOfMonth());
-        System.out.println(yearMonth.isLeapYear());
+        log.info(String.valueOf(yearMonth));
+        log.info(String.valueOf(yearMonth.lengthOfMonth()));
+        log.info(String.valueOf(yearMonth.isLeapYear()));
 
-        System.out.println("-------");
+        log.info("-------");
 
         YearMonth yearMonth1 = YearMonth.of(2016, 2);
-        System.out.println(yearMonth1);
-        System.out.println(yearMonth1.lengthOfMonth());
-        System.out.println(yearMonth1.lengthOfYear());
-        System.out.println(yearMonth1.isLeapYear());
+        log.info(String.valueOf(yearMonth1));
+        log.info(String.valueOf(yearMonth1.lengthOfMonth()));
+        log.info(String.valueOf(yearMonth1.lengthOfYear()));
+        log.info(String.valueOf(yearMonth1.isLeapYear()));
 
-        System.out.println("-------");
+        log.info("-------");
 
         LocalDate localDate7 = LocalDate.now();
         LocalDate localDate8 = LocalDate.of(2018, 3, 16);
 
         Period period = Period.between(localDate7, localDate8);
-        System.out.println(period.getYears());
-        System.out.println(period.getMonths());
-        System.out.println(period.getDays());
+        log.info(String.valueOf(period.getYears()));
+        log.info(String.valueOf(period.getMonths()));
+        log.info(String.valueOf(period.getDays()));
 
-        System.out.println("-------");
+        log.info("-------");
 
-        System.out.println(Instant.now());
+        log.info(String.valueOf(Instant.now()));
         LocalDate d = LocalDate.now();
         System.out.print(d.minusDays(9651).getYear() + "-");
         System.out.print(d.minusDays(9651).getMonthValue() + "-");
         System.out.print(d.minusDays(9651).getDayOfMonth() + 1 + "-");
 
         LocalDate localDate9 = LocalDate.now();
-        System.out.println(localDate9.minusDays(9154).getDayOfMonth());
-        System.out.println(localDate9.minusDays(9154).getMonthValue());
-        System.out.println(localDate9.minusDays(9154).getYear());
+        log.info(String.valueOf(localDate9.minusDays(9154).getDayOfMonth()));
+        log.info(String.valueOf(localDate9.minusDays(9154).getMonthValue()));
+        log.info(String.valueOf(localDate9.minusDays(9154).getYear()));
 
     }
 }
