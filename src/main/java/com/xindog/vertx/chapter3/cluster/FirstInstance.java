9b77193c8e7ba1e4ea23a73ab1998e30dded8965
Shawn Wang <shawang@nvidia.com>
2021-11-23 15:26:57 +0800

    mute netty debug log

--- src/main/java/com/xindog/vertx/chapter3/cluster/FirstInstance.java
+++ src/main/java/com/xindog/vertx/chapter3/cluster/FirstInstance.java
@@ -5,12 +5,19 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.util.logging.LogManager;
 
 public class FirstInstance {
 
     private static final Logger logger = LoggerFactory.getLogger(FirstInstance.class);
 
     public static void main(String[] args) {
+
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+
         Vertx.clusteredVertx(new VertxOptions(), ar -> {
             if (ar.succeeded()) {
                 logger.info("First instance has been started");
